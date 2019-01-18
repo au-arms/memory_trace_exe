625390e4489930997a2c1e2d478844404604da37
Austin <austin@growatomata.com>
2019-01-18 11:24:07 -0700

    first commit. Netbeans skeleton

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aarmstrong
+ *
+ * Created on January 18, 2019, 11:02 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
